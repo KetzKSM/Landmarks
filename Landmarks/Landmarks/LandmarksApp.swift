29d0bab2dfdaef076ec593033067136fb888a37c
Kyle Metz <17kylemetz@gmail.com>
2020-12-17 13:58:56 -0500

    Creating and Combining Views
    Part 1 of SwiftUI tutorial

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kyle Metz on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
